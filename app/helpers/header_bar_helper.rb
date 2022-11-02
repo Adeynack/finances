cbc86b7d99da1190459fb7f7f95d5f02a2352c71
Adeynack <Adeynack@users.noreply.github.com>
2022-11-02 23:47:35 +0100

    better HTML/CSS organisation
    + tentative at BEM

--- app/helpers/header_bar_helper.rb
+++ app/helpers/header_bar_helper.rb
@@ -2,11 +2,11 @@
 
 module HeaderBarHelper
   def nav_item_link_to(text, path, title: nil, strict: false)
-    content_tag "span", class: "nav-item" do
+    content_tag "span", class: "navbar__item" do
       url = url_for(path)
       url_path = URI.parse(url).path
       super_path_of_request = strict ? (url_path == request.path) : /^#{url_path}($|\/).*/i.match?(request.path)
-      link_to text, url, {class: class_names("nav-link", "nav-active": super_path_of_request), title:}
+      link_to text, url, {class: class_names("navbar__link", "navbar__link--active": super_path_of_request), title:}
     end
   end
 end
