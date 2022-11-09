03be1aae4e7b872a7ba4b9f927c97ac1d6ddbe3e
Adeynack <Adeynack@users.noreply.github.com>
2022-11-09 02:13:54 +0100

    wip

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,15 +1,36 @@
 #!/usr/bin/env bash
-set -v
 
 # exit on error
 set -o errexit
 
+echo "<><><><><><><><><><><><><><><><><><><><><><><><><><><>"
+echo "<> bundle install"
+echo "<><><><><><><><><><><><><><><><><><><><><><><><><><><>"
 bundle install
 
+echo "<><><><><><><><><><><><><><><><><><><><><><><><><><><>"
+echo "<> yarn build"
+echo "<><><><><><><><><><><><><><><><><><><><><><><><><><><>"
+# Weirdly enough, even though `assets:precompile` does call
+# yarn build, the app fails to render if it's not called before.
+yarn build
+
+echo "<><><><><><><><><><><><><><><><><><><><><><><><><><><>"
+echo "<> assets:precompile"
+echo "<><><><><><><><><><><><><><><><><><><><><><><><><><><>"
 bundle exec rake assets:precompile
 
+echo "<><><><><><><><><><><><><><><><><><><><><><><><><><><>"
+echo "<> assets:clean"
+echo "<><><><><><><><><><><><><><><><><><><><><><><><><><><>"
 bundle exec rake assets:clean
 
+echo "<><><><><><><><><><><><><><><><><><><><><><><><><><><>"
+echo "<> db:migrate"
+echo "<><><><><><><><><><><><><><><><><><><><><><><><><><><>"
 bundle exec rake db:migrate
 
+echo "<><><><><><><><><><><><><><><><><><><><><><><><><><><>"
+echo "<> db:seed"
+echo "<><><><><><><><><><><><><><><><><><><><><><><><><><><>"
 if [ "$RAILS_PERFORM_SEED" == "1" ]; then bundle exec rake db:seed; fi
