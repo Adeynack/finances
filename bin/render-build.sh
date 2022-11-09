7be3b372b537872ed4df9cb9774ba5137606245c
Adeynack <Adeynack@users.noreply.github.com>
2022-11-09 01:00:25 +0100

    wip

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+# bundle install
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
+echo "<><><><><><><><><><><><><><><><><><><><><><><><>"
+echo "DATABASE_URL:"
+echo $DATABASE_URL
+echo "<><><><><><><><><><><><><><><><><><><><><><><><>"
+echo "RAILS_ENV:"
+echo $RAILS_ENV
+echo "<><><><><><><><><><><><><><><><><><><><><><><><>"
+# bundle exec rake db:migrate
