cb12aec23e238d68b51663a8f172e96ae8403a29
Adeynack <Adeynack@users.noreply.github.com>
2023-05-29 21:16:26 +0200

    crumbs
    + todo helper
    + icons in navigation panel

--- app/helpers/header_bar_helper.rb
+++ app/helpers/header_bar_helper.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 module HeaderBarHelper
-  def nav_item_link_to(text, path, title: nil, strict: false)
-    content_tag "span", class: "navbar__item" do
-      url = url_for(path)
-      url_path = URI.parse(url).path
-      super_path_of_request = strict ? (url_path == request.path) : /^#{url_path}($|\/).*/i.match?(request.path)
-      link_to text, url, {class: class_names("navbar__item--truncated", "navbar__link", "navbar__link--active": super_path_of_request), title:}
+  def crumb(text, target = nil, *link_to_args, **link_to_kwargs)
+    content_tag "li", class: "crumbs__crumb" do
+      if target
+        link_to text, target, *link_to_args, **link_to_kwargs
+      else
+        content_tag "span", text
+      end
     end
   end
 end
