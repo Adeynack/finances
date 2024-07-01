2c8ca3e942d8f2e756b21ad73f2017c4b588bbf4
Adeynack <Adeynack@users.noreply.github.com>
2024-07-01 20:39:08 +0200

    query books

---
+++ app/graphql/types/book_type.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Types
+  class BookType < Types::BaseObject
+    field :id, ID, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :name, String, null: false
+    field :owner_id, ID, null: false
+    field :owner, Types::UserType, null: false
+    field :default_currency_iso_code, String, null: false
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,5 +6,10 @@ module Types
     def me
       context[:current_api_session]&.user
     end
+
+    field :books, [Types::BookType], null: false
+    def books
+      Book.all # todo: Secure this to only the books accessible by the logged in user.
+    end
   end
 end
