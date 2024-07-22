26cffa380db259ad5620c87e9ac1ae77ee0008d9
Adeynack <Adeynack@users.noreply.github.com>
2024-07-22 20:15:39 +0000

    switch to top-bar menu

--- client/src/components/core/MainLayout.tsx
+++ client/src/components/core/MainLayout.tsx
@@ -1,68 +1,74 @@
-import { Layout, Menu } from "antd";
-import { Content } from "antd/es/layout/layout";
-import Sider from "antd/es/layout/Sider";
+import { Layout, Menu, theme } from "antd";
+import { Content, Header } from "antd/es/layout/layout";
 import { BookList } from "../../BookList";
 import { ThemeSwitch } from "./ThemeSwitch";
-import React, { useState } from "react";
-import { AccountBookOutlined, BookOutlined, FolderOutlined, MenuFoldOutlined, MenuUnfoldOutlined, UserOutlined } from "@ant-design/icons";
+import React from "react";
+import { AccountBookOutlined, BookOutlined, FolderOutlined, UserOutlined } from "@ant-design/icons";
 
 const layoutStyle: React.CSSProperties = {
   margin: -8
-};
-
-const siderStyle: React.CSSProperties = {
-  height: '100vh',
 };
 
 const contentStyle: React.CSSProperties = {
   margin: "8px"
 };
 
+const headerStyle: React.CSSProperties = {
+  paddingLeft: 0,
+  paddingRight: 0,
+  display: 'flex',
+  width: '100%',
+};
+
+const menuStyle: React.CSSProperties = {
+  flex: 1,
+  minWidth: 0,
+}
+
 export default function MainLayout() {
-  const [collapsed, setCollapsed] = useState(true);
+  const { token: { colorBgContainer } } = theme.useToken();
+
+  const titleStyle: React.CSSProperties = {
+    backgroundColor: colorBgContainer,
+  }
 
   return (
     <Layout style={layoutStyle}>
-      <Sider style={siderStyle} collapsed={collapsed}>
+      <Header style={headerStyle}>
+        <div style={titleStyle}>
+          💰 Finances
+        </div>
         <Menu
+          style={menuStyle}
+          mode="horizontal"
           items={[
             {
-              key: 'title',
-              label: <>
-                <span>💰</span>
-                {!collapsed && <span> Finances</span>}
-              </>,
-              type: "group"
-            },
-            {
-              key: 'collapse-bar',
-              label: collapsed ? 'Display menu' : 'Collapse menu',
-              icon: collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />,
-              onClick: () => setCollapsed(!collapsed)
+              key: 'books',
+              label: 'Book',
+              icon: <BookOutlined />,
+              title: "Book's root & other books",
             },
             {
               key: 'accounts',
               label: 'Accounts',
-              icon: <AccountBookOutlined />
+              icon: <AccountBookOutlined />,
+              title: "Current book's accounts",
             },
             {
               key: 'categories',
               label: 'Categories',
-              icon: <FolderOutlined />
+              icon: <FolderOutlined />,
+              title: "Current book's categories",
             },
             {
-              key: 'books',
-              label: 'Books',
-              icon: <BookOutlined />
-            },
-            {
-              key: 'settings',
-              label: 'User & Settings',
-              icon: <UserOutlined />
+              key: 'user',
+              label: 'User',
+              icon: <UserOutlined />,
+              title: "User settings"
             }
           ]}
         />
-      </Sider>
+      </Header>
       <Content style={contentStyle}>
         <ThemeSwitch />
         <BookList />
